c007a9a9c29356b53029987120e52392473cc032
Luha Yang <luhayang@Luhas-Laptop.local>
2024-01-24 14:55:08 -0600

    Edited tables

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
   quakeTime timestamp with time zone,
-  latitude real,
-  longitude real,
-  quakeDepth real,
-  magnitude real,
+  latitude float,
+  longitude float,
+  quakeDepth float,
+  magnitude float,
   seismicNet char,
   place text,
-  quakeType text
+  quakeType char
 );
