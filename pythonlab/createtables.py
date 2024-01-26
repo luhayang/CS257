0f6dc617a479749e786261fd5a0762a5ef54e4d8
luhayang <yangl4@stearns.mathcs.carleton.edu>
2024-01-26 10:20:22 -0500

    updated

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,14 +1,24 @@
+import psycopg2
+
+def test_connection():
+	
+conn = psycopg2.connect("yangl4")
+cur = conn.cursor()
+cur.execute("SELECT * FROM cities")
+cur.execute("SELECT * FROM states")
+conn.commit()
+
 DROP TABLE IF EXISTS states;
-CREATE TABLE states {
+CREATE TABLE states (
   state text,
   abbreviation text
-};
+);
 
 DROP TABLE IF EXISTS cities;
-CREATE TABLE cities {
+CREATE TABLE cities (
   city text,
   state text,
   population int,
   lat double,
   lon double
-};
+);
