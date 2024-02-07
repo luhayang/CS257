7fb323040ac864b25d2cfea8d68d3d76695dbac9
luhayang <yangl4@stearns.mathcs.carleton.edu>
2024-02-07 10:33:50 -0500

    updated

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -1,4 +1,5 @@
 import flask
+import psycopg2
 
 app = flask.Flask(__name__)
 
@@ -14,8 +15,21 @@ def my_display(word1, word2):
 
 @app.route('/color/<word1>')
 def my_color(word1):
-    return '<h1 style="color:Red">' + word1 + '</h1>'
+    return '<h1 style="color:SlateBlue">' + word1 + '</h1>'
 
+@app.route('/add/<num1>/<num2>')
+def my_add(num1, num2):
+    the_sum = int(num1) + int(num2)
+    the_string = "The sum of " + num1 + " and " + num2 + " is " + str(the_sum);
+    return the_string
+	
+@app.route('/area/<abbrev>')
+def my_area(abbrev):
+    conn = psycopg2.connec(host="localhost", port = 5432, database="yangl4", user="yangl4", password="stars929bond")
+    cur = conn.cursor()
+    SELECT * FROM states WHERE abb = abbrev;
+
+def get_area(
 if __name__ == '__main__':
     my_port = 5137
     app.run(host='0.0.0.0', port = my_port)
