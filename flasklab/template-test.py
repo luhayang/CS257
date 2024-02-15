683c7ace689dc1510e301937a818889bad6f356d
luhayang <luhayang@Luhas-Laptop.local>
2024-02-15 11:10:21 -0600

    updated

--- flasklab/template-test.py
+++ flasklab/template-test.py
@@ -40,7 +40,7 @@ def my_city():
 
     cities = cur.fetchall()
     idx = random.randint(0, len(cities)-1)
-    city = ''+cities[idx]
+    city = ''+cities[idx][0]
 
     conn.commit()
     cur.close()
